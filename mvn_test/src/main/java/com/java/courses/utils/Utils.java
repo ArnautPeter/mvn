599fa248a4c0734761bc5cb65780034dbc210f2c
Peter Arnaut <p.arnaut@mail.ru>
2016-06-09 21:13:35 +0300

    mvn

---
+++ mvn_test/src/main/java/com/java/courses/utils/Utils.java
@@ -0,0 +1,12 @@
+package com.java.courses.utils;
+
+
+public class Utils {
+
+    public void printArray(int[] array) {
+
+        for (int i : array){
+            System.out.println(i + ",");
+        }
+    }
+}
---
+++ mvn_usage/src/main/java/com/java/utils/usage/UtilsTest.java
@@ -0,0 +1,14 @@
+package com.java.utils.usage;
+
+
+import org.joda.time.DateTime;
+
+public class UtilsTest {
+
+
+    public static void main(String[] args) {
+
+        DateTime dateTime = new DateTime();
+        System.out.println(dateTime.getDayOfMonth());
+    }
+}
